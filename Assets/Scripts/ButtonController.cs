a13b065a4daa0b31539ae38486d51146ca4753fd
davashikashi <deison.cardona@correounivalle.edu.co>
2024-04-16 20:37:49 -0500

    GUI añadida

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    public GameObject Inicio;
+    public GameObject Running;
+    public GameObject Pause;
+    // Start is called before the first frame update
+
+    public void StartGame()
+    {
+        Running.SetActive(true);
+        Inicio.SetActive(false);  
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                // Si estamos en una compilación, cerramos la aplicación
+                Application.Quit();
+        #endif
+    }
+
+    public void PauseGame(){
+        Time.timeScale = 0f; // Pausar el juego
+        Pause.SetActive(true);   
+        Running.SetActive(false);
+    }
+
+    public void ResumeGame(){
+        Time.timeScale = 1f;
+        Running.SetActive(true);
+        Pause.SetActive(false);
+    }
+
+    public void MenuGame(){
+        Inicio.SetActive(true);
+        Running.SetActive(false);
+        Pause.SetActive(false);
+    }
+}
